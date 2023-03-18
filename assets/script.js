fff99effe455e3bf8fae9ebd88147a68fa4a3fde
Jack Smith <jack_smith21554@yahoo.com>
2023-03-17 20:05:45 -0400

    Linked bootstrap and jquery in index.html file. Commented out general html structure and javascript intended functions.

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// Declare html elements (by id [#]) as variables
+
+
+// Create a timer function
+
+
+// Create questions and answeres as variables
+
+
+// Create loop to ask questions randomly until all have been asked or timer ends
+
+    // Create empty variables for scoring
+
+    // Us if and else statements to check if user selected correct answer
+
+
+// Create end screen displaying score w/ option to input intials; Save score to local storage
+
+    
