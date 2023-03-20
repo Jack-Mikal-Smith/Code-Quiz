fd1fba8857b2146924d1c398091835ff23ee8835
Jack Smith <jack_smith21554@yahoo.com>
2023-03-20 15:26:45 -0400

    Created variables in js that link to sections of the html file. Declared starting variables for the timer and question index. Created an array of questions with specific values for prompt, possible answers, and the correct answer. Added event listner to the start button; starts the timer and display questions functions and switches which body element is shown. Declared timer function as a variable to count down one second at a time and displays it in the nav bar. Declared display questions function as a variable; inputs question array values into cooresponding game area elements. Added event listner to all of the possible answer buttons; cycles questions if there are any left on the array; subtracts 5 seconds for incorrect answers.

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,87 @@
 // Declare html elements (by id [#]) as variables
+var scoreList = $('#scores-lnk');
+var gameArea = $('#game-area');
+var startBtn = $('#start-btn');
+var timerEl = $('#timer');
+var startArea = $('#start-area');
+
+// Declare variables starting variables for the timer and question index
+var questionIndex = 0;
+var timeLeft = 60;
+
+// Declaring an array of the potential questions as a variable
+var question = [
+    {
+        question: "PlaceHolder",
+        choices: ["a", "b", "c", "d"],
+        correct: "a"
+    },
+    {
+        question: "PlaceHolder2",
+        choices: ["a", "b", "c", "d"],
+        correct: "a"
+    },
+    {
+        question: "PlaceHolder3",
+        choices: ["a", "b", "c", "d"],
+        correct: "a"
+    },
+];
 
 
-// Create a timer function
+// Adding event listner to start button
+startBtn.on('click', function() {
+    startArea.addClass('visually-hidden');
+    gameArea.removeClass('visually-hidden');
+    timerFun();
+    displayQuestion();
+})
+
+// Declaring timer function as a variable
+var timerFun = function() {
+    timer=setInterval(function() {
+        if (timeLeft > 0) {
+            timeLeft--
+            timerEl.text(timeLeft);
+        }
+    },1000)
+}
+
+// Declaring dispaly question as a variable; inputs question array values into cooresponding game-area elements
+var displayQuestion = function() {
+    $("#prompt").text(question[questionIndex].question);
+    $("#a-btn").text(question[questionIndex].choices[0]);
+    $("#b-btn").text(question[questionIndex].choices[1]);
+    $("#c-btn").text(question[questionIndex].choices[2]);
+    $("#d-btn").text(question[questionIndex].choices[3]);
+}
+
+// Adding event listner to all of the game-area buttons; if incorrect choice is selected, time is reduced by 5 sec;
+// Hides game-area if all questions in the array have been displayed
+gameArea.on('click', "button", function() {
+    var choice = $(this).text();
+    var correct = question[questionIndex].correct;
+    if (choice !== correct) {
+        console.log(correct);
+        timeLeft-=5;
+    } 
+    questionIndex++;
+    if (questionIndex === question.length) {
+        gameArea.addClass("visually-hidden");
+        clearInterval(timer);
+        return;
+    }
+    displayQuestion();
+})
 
 
-// Create questions and answeres as variables
+// Create empty variables for scoring
 
 
-// Create loop to ask questions randomly until all have been asked or timer ends
-
-    // Create empty variables for scoring
-
-    // Us if and else statements to check if user selected correct answer
 
 
-// Create end screen displaying score w/ option to input intials; Save score to local storage
+// Create win screen displaying score w/ option to input intials; Save score to local storage
+
+// Create lose screen with option to play agian
 
     
